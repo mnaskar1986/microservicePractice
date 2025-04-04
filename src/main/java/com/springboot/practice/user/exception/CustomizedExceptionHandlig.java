ac43a7f86f65db82273e7112cee7eead58c9a5ac
mnaskar1986 <mnaskar1986@gmail.com>
2025-04-04 16:24:14 -0700

    Pushing code to add validation for fields.

--- src/main/java/com/springboot/practice/user/exception/CustomizedExceptionHandlig.java
+++ src/main/java/com/springboot/practice/user/exception/CustomizedExceptionHandlig.java
@@ -2,8 +2,11 @@ package com.springboot.practice.user.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,5 +22,19 @@ ResponseEntityExceptionHandler{
 		ErrorDetails error = new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now());
 		return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest req) throws Exception {
+		
+		ErrorDetails error = new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now());
+		return new ResponseEntity<ErrorDetails>(error, HttpStatus.NOT_FOUND);
+	}
 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest req) {
+		
+		ErrorDetails error = new ErrorDetails(ex.getFieldError().getDefaultMessage(), req.getDescription(false), LocalDateTime.now());
+		return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+	}
 }
